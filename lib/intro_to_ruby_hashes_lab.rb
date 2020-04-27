edb3780829e3934480f5fd459689d6fabd12e972
Andre Carter <carstar2486@gmail.com>
2020-04-27 14:55:22 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,13 +1,19 @@
 def new_hash
   # return an empty hash
+  x = Hash.new;
+  return x
 end
 
 def my_hash
   # return a valid hash with any key/value pair of your choice
+  x = {:name=>"Andre"};
+  return x
 end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  x = {:name=>"Grace Hopper"};
+  return x
 end
 
 
