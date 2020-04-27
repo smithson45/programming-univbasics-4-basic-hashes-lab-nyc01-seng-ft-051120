736d39c15bb90468b9e6c9d7300ce52e12dc0ec7
Andre Carter <carstar2486@gmail.com>
2020-04-27 14:57:09 +0000

    Done.

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,16 +1,24 @@
 def new_hash
   # return an empty hash
+  x = Hash.new;
+  return x
 end
 
 def my_hash
   # return a valid hash with any key/value pair of your choice
+  x = {:name=>"Andre"};
+  return x
 end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  x = {:name=>"Grace Hopper"};
+  return x
 end
 
 
 def id_hash_generator(number)
   # return a hash with a key :id assigned to the provided number
+  x = {:id=>number};
+  return x
 end
